3018dbbd9f6a6e22a5618c345cd81ce99c1bc976
ChrisManin <tof.giga@hotmail.fr>
2020-05-08 14:07:15 +0200

    mise en place des city-cards

--- src/App.vue
+++ src/App.vue
@@ -2,20 +2,34 @@
   <div id="app">
     <div class="container">
       <h1>Mesure de la qualité de l'air</h1>
+      <div class="row">
+        <div v-for="city of cities" :key="city.index" class="col-sm-4">
+          <City :city="city" />
+        </div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import City from "@/components/City";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-
+    City
+  },
+  data() {
+    return {
+      cities: [
+        { name: "Lyon", iqa: 40 },
+        { name: "Paris", iqa: 70 },
+        { name: "Grenoble", iqa: 10 }
+      ]
+    };
   }
-}
+};
 </script>
 
 <style>
-
 </style>
