f54f4b92e611f58901cf2331004775a6f5a461e6
ChrisManin <tof.giga@hotmail.fr>
2020-05-10 17:56:15 +0200

    Ecriture dans la bdd

--- src/components/pages/Admin.vue
+++ src/components/pages/Admin.vue
@@ -1,11 +1,47 @@
 <template>
   <div>
     <h1>Admin</h1>
+    <div class="row form-group ajouterVille">
+      <div class="col-sm-2">Ajouter une ville :</div>
+
+      <div class="col-sm-3">
+        <input type="text" id="cityField" class="form-control" v-model="newCity" />
+      </div>
+
+      <div class="col-sm-2">
+        <b-button @click="addCityAction">Ajouter</b-button>
+      </div>
+    </div>
+
+    <ul>
+      <li v-for="city of cities" :key="city.id">{{ city.name }}</li>
+    </ul>
   </div>
 </template>
 
 <script>
-export default {};
+import { CitiesService } from "@/services/Cities.service";
+
+export default {
+  data() {
+    return {
+      cities: [],
+      newCity: ""
+    };
+  },
+  async mounted() {
+    this.cities = await CitiesService.getCities();
+    console.log(this.cities);
+  },
+  methods: {
+    async addCityAction() {
+      const city = { name: this.newCity, iqa: 0 };
+      const idCity = await CitiesService.addCity(city);
+      city.id = idCity;
+      this.cities.push(city);
+    }
+  }
+};
 </script>
 
 <style lang="scss" scoped>
--- src/services/Cities.service.js
+++ src/services/Cities.service.js
@@ -5,8 +5,15 @@ export const CitiesService = {
     const citiesRef = db.collection("cities");
     const citiesDoc = await citiesRef.get();
 
-    const cities = citiesDoc.docs.map((city) => city.data());
+    const cities = citiesDoc.docs.map((city) =>
+      Object.create({ ...city.data(), id: city.id })
+    );
 
     return cities;
+  },
+  async addCity(city) {
+    const citiesRef = db.collection("cities");
+    const result = await citiesRef.add(city);
+    return result.id;
   },
 };
